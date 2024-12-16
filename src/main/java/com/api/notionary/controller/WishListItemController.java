56ebd51a0d95042e20d99dc63a33af573a10cb03
Oleksandr Yastriebov <oyastriebov@techmatesgroup.com>
2024-12-17 00:37:44 +0200

    Refactoring

--- src/main/java/com/api/notionary/controller/WishListItemController.java
+++ src/main/java/com/api/notionary/controller/WishListItemController.java
@@ -12,11 +12,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.net.URI;
-import java.util.List;
 
 @Controller
 @RequestMapping("/api/wishlist")
@@ -41,7 +41,7 @@ public class WishListItemController {
         String userEmail = userDetails.getUsername();
 
         if (!wishlistService.isWishlistOwner(wishlistId, userEmail)) {
-            return ResponseEntity.status(403).body("You need to be owner add a wishlist items.");
+            return ResponseEntity.status(403).body("You need to be owner to add a wishlist items.");
         }
 
         WishlistItemDto savedItem = wishListItemService.addWishListItem(wishlistId, wishlistItemDto);
@@ -58,7 +58,7 @@ public class WishListItemController {
     }
 
     @DeleteMapping("/{wishlistId}/wish/{itemId}")
-    public ResponseEntity<?> deleteWishListItem(@PathVariable String wishlistId, @PathVariable String itemId,
+    public ResponseEntity<String> deleteWishListItem(@PathVariable String wishlistId, @PathVariable String itemId,
                                                 Authentication authentication) {
         if (authentication == null || !authentication.isAuthenticated()) {
             return ResponseEntity.status(401).body("You need to be authorised to delete a wishlist item.");
@@ -72,5 +72,20 @@ public class WishListItemController {
         wishListItemService.deleteWishlistItem(wishlistId, itemId);
         return ResponseEntity.ok().body(
                 String.format("Wishlist item with id: %s was successfully removed from the wishlist: %s", itemId, wishlistId));
+    }
+
+    @PutMapping("/{wishlistId}/wish/{itemId}")
+    public ResponseEntity<?> updateWishlistItem(@PathVariable String wishlistId, @PathVariable String itemId,
+                                                @RequestBody WishlistItemDto wishlistItemDto, Authentication authentication) {
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return ResponseEntity.status(401).body("You need to be authorised to update a wishlist item.");
+        }
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String userEmail = userDetails.getUsername();
+
+        if (!wishlistService.isWishlistOwner(wishlistId, userEmail)) {
+            return ResponseEntity.status(403).body("You need to be owner to update a wishlist item.");
+        }
+        return ResponseEntity.ok().body("SDf");
     }
 }
