f10a5955e222b1a3bfcca40d94d40deab386e951
s1t1a1w1r1@gmail.com <oyastriebov@techmatesgroup.com>
2026-03-15 01:45:33 +0200

    Remove FE project

--- notionary-ui.frontend/src/api/userApi.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "./axiosConfig";
-
-export const userApi = {
-  getCurrentUser: () => axiosInstance.get("/users/me"),
-
-  updateUser: (data) => axiosInstance.patch("/users/me", data),
-
-  deleteUser: (userId) => axiosInstance.delete(`/users/${userId}`),
-};
